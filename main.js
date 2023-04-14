b65af88b13a275a5a879ad9d4a6868c0d88bd2d4
nsx_69 <mrmanafest16@gmail.com>
2023-04-14 11:47:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var elSwitchButton = document.querySelector(".header-link-wrapper");
+
+
+elSwitchButton.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("dark-theme");
+});
